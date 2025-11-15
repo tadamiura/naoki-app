f2ea0d248dde5f5c642a68ec7f904e5dacbe2ee3
Tadatoshi <tmiura@DESKTOP-U42RHRI.localdomain>
2025-11-15 11:29:37 +0100

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+   public function boot(): void
+{
+    if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
     }
+}
 }
